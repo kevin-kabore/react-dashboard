94cb23dbf3a866b8a3e23c4a41991f45e67c676b
Kevin Kabore <kevin.s.kabore@gmail.com>
2017-06-15 16:51:38 -0700

    creates and renders main component:

---
+++ app/components/DashboardApp.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class DashboardApp extends Component {
+  render() {
+    return(
+      <div>Dashboard App</div>
+    )
+  }
+}
+
+module.exports = DashboardApp
